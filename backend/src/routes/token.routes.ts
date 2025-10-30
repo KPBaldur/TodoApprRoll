0ff35b9922a847fadbc6ea3ccf473be9531b41b2
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-10-30 09:17:38 -0300

    Delete token.routes.ts

--- backend/src/routes/token.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { refreshAccessToken, logout } from "../controllers/token.controller";
-
-const router = express.Router();
-
-router.post("/refresh", refreshAccessToken);
-router.post("/logout", logout);
-
-export default router;
