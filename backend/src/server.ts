7881a63bd73a5709008f6cb06bd60a4b86da6d4d
BaldurDev <kevin.pizarrosanz@gmail.com>
2025-11-03 11:52:34 -0300

    reconfiguracion de cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -39,7 +39,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+app.options(/.*/, cors(corsOptions));
 
 app.use(helmet());
 app.use(express.json());
